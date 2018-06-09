35a52c7d0d8502a21fa82e5976a990a3c9b20427
anyoneortwo <424057214@qq.com>
2018-06-09 20:28:30 +0800

    5

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 var config = {
 	chainId: 1,
 	apiPrefix: "https://mainnet.nebulas.io", //https://testnet.nebulas.io  https://mainnet.nebulas.io
-	contractAddr: "n1ynV59YNV6eAs1qpV9qRMsKhrSgeipCAkP", //main n1zsohpv63CnmUs7aeVBfgeQBozpK5bmTMk test n21iah7tXxgxTZ6R81n9UuWGwULcuVansBd
+	contractAddr: "n1owFAf3H5htvjBYRLFh7BhEiui2q7q7rzU", //main n1zsohpv63CnmUs7aeVBfgeQBozpK5bmTMk test n21iah7tXxgxTZ6R81n9UuWGwULcuVansBd
 	myAddress: "n1JeDTMq5xHq6Y16yApYbMdT4Vw4K9kzbK9",
 	gaslimit: 2000000,
 	gasprice: 1000000,
